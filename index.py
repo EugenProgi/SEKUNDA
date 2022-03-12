c5855c9c986c22595d573e88a9a5cfa08cefc0d0
EugenProgi <101456854+EugenProgi@users.noreply.github.com>
2022-03-12 16:55:10 +0200

    Update index.py

--- index.py
+++ index.py
@@ -19,3 +19,13 @@ class Block(object):
 
   def __str__(self):
     return "Block<prev_hash: %s,hash: %s>" % (self.prev_hash, self.hash)
+#check if chaindata folder exists.
+chaindata_dir = 'chaindata'
+if not os.path.exists(chaindata_dir):
+  #make chaindata dir
+  os.mkdir(chaindata_dir)
+  #check if dir is empty from just creation, or empty before
+if os.listdir(chaindata_dir) == []:
+  #create first block
+  first_block = create_first_block()
+  first_block.self_save()
